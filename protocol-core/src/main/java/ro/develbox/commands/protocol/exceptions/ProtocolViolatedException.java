b1d716aebe2edbf832a808ecbee790f02ed875e5
danielv <daniel@home.com>
2017-06-04 19:58:52 +0300

    remove some duplicate code

--- protocol-core/src/main/java/ro/develbox/commands/protocol/exceptions/ProtocolViolatedException.java
+++ protocol-core/src/main/java/ro/develbox/commands/protocol/exceptions/ProtocolViolatedException.java
@@ -1,5 +1,7 @@
 package ro.develbox.commands.protocol.exceptions;
 
+import ro.develbox.annotation.StartCommand;
+import ro.develbox.annotation.TerminalCommand;
 import ro.develbox.commands.Command;
 
 public abstract class ProtocolViolatedException extends Exception {
@@ -23,19 +25,29 @@ public abstract class ProtocolViolatedException extends Exception {
         if (command == null) {
             return cause;
         }
-        String commandDesc = describeCommand(command);
-        String curretnCommandMessage = "Comand : " + command.getCommand() + "violates protocol. Received "
-                + " with annotation :" + commandDesc;
+        String curretnCommandMessage = "Comand : " + command.getCommand() + "violates protocol. Received : "
+                + commandDescription(command);
         String secondPartMessage = "\n\t\t" + "Previously command";
 
         if (prevCommand != null) {
-            secondPartMessage = prevCommand.getCommand() + " with annotation " + describeCommand(prevCommand);
+            secondPartMessage = commandDescription(prevCommand);
         } else {
             secondPartMessage = secondPartMessage + " NULL";
         }
         return cause + " \n\t" + curretnCommandMessage + secondPartMessage;
     }
 
+    private String commandDescription(Command command){
+        return command.getCommand() + " annotation :" + describeCommand(command) + "(start : "+isStartCommand(command)+", terminal : "+isTerminalCommand(command)+")";
+    }
+    
     protected abstract String describeCommand(Command command);
+    
+    private boolean isStartCommand(Command command){
+        return command.getClass().getAnnotation(StartCommand.class)!=null;
+    }
 
+    private boolean isTerminalCommand(Command command){
+        return command.getClass().getAnnotation(TerminalCommand.class)!=null;
+    }
 }
