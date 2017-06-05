73a82ba012b851aa85749a6a3b60bfed322d62bd
danielv <daniel@home.com>
2017-06-05 21:53:02 +0300

    Annotations for CommandAuth

--- protocol/src/main/java/ro/develbox/commands/CommandAuth.java
+++ protocol/src/main/java/ro/develbox/commands/CommandAuth.java
@@ -1,13 +1,21 @@
 package ro.develbox.commands;
 
+import ro.develbox.annotation.ClientCommand;
 import ro.develbox.annotation.ServerCommand;
+import ro.develbox.annotation.StartCommand;
+import ro.develbox.annotation.TerminalCommand;
 
 /**
- * Command sent to authenticate the client ( validate that client is using this client)
+ * Command sent to authenticate the client/server
  * @author danielv
  *
  */
-@ServerCommand
+//response to command is a same type command 
+//this is the start and the end of the command succession
+@ServerCommand(responseCommandType={CommandAuth.class})
+@ClientCommand(responseCommandType={CommandAuth.class})
+@StartCommand
+@TerminalCommand
 public abstract class CommandAuth extends Command {
 
     public static final String COMMAND = "auth:";
