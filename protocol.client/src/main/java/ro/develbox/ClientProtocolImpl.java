a8d6b1f6c8103b2ce566a7e99e3c75397b3003a4
danielv <daniel@home.com>
2017-06-08 23:12:44 +0300

    work in progress

--- protocol.client/src/main/java/ro/develbox/ClientProtocolImpl.java
+++ protocol.client/src/main/java/ro/develbox/ClientProtocolImpl.java
@@ -1,20 +1,27 @@
 package ro.develbox;
 
+import ro.develbox.commands.Command;
 import ro.develbox.commands.CommandLogin;
 import ro.develbox.protocol.ICommandSender;
 import ro.develbox.protocol.IProtocolResponse;
 import ro.develbox.protocol.client.ClientProtocol;
-public class ClientProtocolImpl extends ClientProtocol{
+public class ClientProtocolImpl extends ClientProtocol implements IProtocolResponse{
 
 	public ClientProtocolImpl(IProtocolResponse responder, ICommandSender sender) {
 		super(responder, sender);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public void login(String email){
 		CommandLogin login = (CommandLogin)commandConstructor.createCommandInstance(CommandLogin.COMMAND);
 		login.setEmail(email);
 		login.setPassword(email);
+	}
+
+	@Override
+	public Command getCommandResponse(Command command) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
